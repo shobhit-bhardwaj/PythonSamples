eaddddf4c8855e232df92e6a4c36883e9ba9b45c
A1000891 <A1000891@AINLH5CG64933BZ.hewitt.com>
2018-12-04 18:05:47 +0530

    NumPy - Simple Operations

---
+++ com/shobhit/numpy/operations.py
@@ -0,0 +1,22 @@
+'''
+@author: Shobhit Bhardwaj
+'''
+
+import numpy as np
+
+arr = np.arange(0,11)
+print(arr)
+
+print(arr + arr)
+print(arr - arr)
+print(arr * arr)
+
+print(arr + 10)
+print(arr - 10)
+print(arr * 10)
+
+print(np.sqrt(arr))
+print(np.exp(arr))
+
+print(np.sin(arr))
+print(np.log(arr))
