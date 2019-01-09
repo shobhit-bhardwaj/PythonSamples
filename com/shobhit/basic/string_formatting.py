24e9908a6db6901ab69d364ea742ce76023340f2
A1000891 <A1000891@AINLH5CG64933BZ.hewitt.com>
2019-01-09 13:46:08 +0530

    Basic - String Formatting

---
+++ com/shobhit/basic/string_formatting.py
@@ -0,0 +1,18 @@
+'''
+@author: Shobhit Bhardwaj
+'''
+
+print('My Name is - {}. I am an {}.'.format('Shobhit Bhardwaj', 'Engineer'))
+
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+print('The {0} {0} {0}'.format('fox', 'brown', 'quick'))
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+result = 100/7
+print('The Result is - {r:1.3f}'.format(r=result))
+
+name = 'Shobhit'
+salary = 17000
+print(f'Hello, My name is {name}. My salary is {salary}.')
+print(f'The Result is - {result:1.2f}')
